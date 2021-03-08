ed396480c63e04a88c035c079adbe8b851ea70ab
melih1756 <76849593+melih1756@users.noreply.github.com>
2021-03-08 16:33:00 +0300

    Update install2.sh

--- scripts/install2.sh
+++ scripts/install2.sh
@@ -58,7 +58,7 @@ upload_proxy() {
 }
 gen_data() {
     seq $FIRST_PORT $LAST_PORT | while read port; do
-        echo "zxcv123123/zxcv123123/$IP4/$port/$(gen64 $IP6)"
+        echo "melih/melih/$IP4/$port/$(gen64 $IP6)"
     done
 }
 
